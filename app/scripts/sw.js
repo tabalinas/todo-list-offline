e08f0a92a6f731acf1928bfaa52a90fa7dff0b65
Artem Tabalin <tabalinas@gmail.com>
2016-04-25 20:58:15 +0200

    SW: Implement basic prefetch caching strategy

--- app/scripts/sw.js
+++ app/scripts/sw.js
@@ -1,1 +1,26 @@
-// Empty ServiceWorker
+
+var CACHE_NAME = 'todo-list-offline-v1';
+var urlsToCache = [
+    '/'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
