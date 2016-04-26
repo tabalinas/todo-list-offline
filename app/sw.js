8e37d5de88d5bf24ca3d7879be0330c5a4bc30ff
Artem Tabalin <tabalinas@gmail.com>
2016-04-26 09:35:19 +0200

    SW: Implement basic example

--- app/sw.js
+++ app/sw.js
@@ -1,25 +1,30 @@
 
 var CACHE_NAME = 'todo-list-offline-v1';
 var urlsToCache = [
-    '/'
+    '/todo-list-offline/app/another.html'
 ];
 
 self.addEventListener('install', function(event) {
+    console.log("installing");
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(function(cache) {
+                console.log("cache opened");
                 return cache.addAll(urlsToCache);
             })
     );
 });
 
 self.addEventListener('fetch', function(event) {
+    console.log("fetching");
     event.respondWith(
         caches.match(event.request)
             .then(function(response) {
+                console.log("cache hit");
                 if (response) {
                     return response;
                 }
+
                 return fetch(event.request);
             })
     );
